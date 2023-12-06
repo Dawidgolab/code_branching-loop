f8288295ad6b55c62ca5cba363567710a4f7b888
Dawidgolab <dawidg122@interia.pl>
2023-12-06 16:47:55 +0100

    help Function

--- Tasks/NumberGuessingGame_ComputerGuesser.py
+++ Tasks/NumberGuessingGame_ComputerGuesser.py
@@ -1,6 +1,26 @@
 import random
 
-    
+def help(computerGuessNumber,myNumber):
+    while True:
+        print("Please human help me!!!!")
+        help = input("Help the computer? (y/n): ")
+        if help == 'y':
+            if computerGuessNumber < myNumber:
+                print("too low")
+            elif computerGuessNumber > myNumber:
+                print("too big")
+            break
+        elif help == 'n':
+            print("Okey so i will do it myself!!!")
+            break
+        else:
+            print("Wrong options. Try again!")
+        continue
+
+
+
+
+
 
 myNumber = int(input("Give the number in range from 1 to 100: "))
 computerGuessNumber = random.randint(1,100)
@@ -11,6 +31,9 @@ while computerGuessNumber != myNumber:
     print("------------------------------------------------------------")
     trials += 1
     computerGuessNumber = random.randint(1,100)
+
+    if trials == 10:
+        help(computerGuessNumber,myNumber)
 
     if computerGuessNumber < myNumber:
         continue
