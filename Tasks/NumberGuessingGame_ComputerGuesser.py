6f2ef53769e3e4e201e4086dc2cdb8b49b6df39f
Dawidgolab <dawidg122@interia.pl>
2023-12-06 17:20:45 +0100

    Comments

--- Tasks/NumberGuessingGame_ComputerGuesser.py
+++ Tasks/NumberGuessingGame_ComputerGuesser.py
@@ -1,23 +1,24 @@
 import random
 
-def too_high(high,computerGuessNumber):
+def too_high(high,computerGuessNumber): # range too high we reduce by -1
     high = computerGuessNumber - 1
     print("too high")
     print("------------------------------------------------------------")
     return high
-def too_low(low,computerGuessNumber):
+def too_low(low,computerGuessNumber): # range too low we reduce by +1
     low = computerGuessNumber + 1
     print("too low")
     print("------------------------------------------------------------")
     return low
 
 myNumber = int(input("Give the number in range from 1 to 100: "))
-low = 0
-high = 100
+
+low = 0 # lower range
+high = 100 # higher range
 trials = 0
 
 while True:
-    computerGuessNumber = random.randint(low,high)
+    computerGuessNumber = random.randint(low,high) # computer random number
     trials += 1
     print(f"Attempt:{trials}. I'm thinking .....{computerGuessNumber}?")
 
